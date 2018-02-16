6d650c20c03710af4a67159e346ad4f19696b6c2
Alex Eng <aeng@redhat.com>
2018-02-16 15:44:31 +1000

    WIP: jsf dependency

--- frontend/src/app/containers/Info/index.tsx
+++ frontend/src/app/containers/Info/index.tsx
@@ -3,22 +3,16 @@ import { connect } from 'react-redux';
 import { RootState } from '../../reducers';
 import { getInfo } from '../../actions/info';
 
-export namespace Info {
-  export interface Props {
-    name?: string,
-    version: string,
-    build?: string,
-    dev?: boolean
-    handleGetInfo: typeof getInfo;
-  }
-
-  export interface State {
-    /* empty */
-  }
+export interface Props {
+  name?: string,
+  version: string,
+  build?: string,
+  dev?: boolean
+  handleGetInfo: typeof getInfo;
 }
 
 @connect(mapStateToProps, mapDispatchToProps)
-export class Info extends React.Component<Info.Props, Info.State> {
+export class Info extends React.Component<Props, {}> {
 
   public componentDidMount () {
     this.props.handleGetInfo()
@@ -28,41 +22,35 @@ export class Info extends React.Component<Info.Props, Info.State> {
     const { name, version, build, dev } = this.props;
 
     return (
-      <div className='d-flex justify-content-center'>
-        <div className='p-3 mb-2 mt-2'>
+      <div className='container justify-content-center w-50'>
+        <h1>Information</h1>
+        <div className='p-3 mt-3'>
           <table className='table'>
-            <thead>
-            <tr>
-              <th colSpan={2} className='text-center'>
-                Information
-              </th>
-            </tr>
-            </thead>
             <tbody>
-            <tr>
-              <td>Name</td>
-              <td>
-                <span className='badge badge-info'>{name}</span>
-              </td>
-            </tr>
-            <tr>
-              <td>Version</td>
-              <td>
-                <span className='badge badge-info'>{version}</span>
-              </td>
-            </tr>
-            <tr>
-              <td>Build</td>
-              <td>
-                <span className='badge badge-info'>{build}</span>
-              </td>
-            </tr>
-            { dev && <tr>
-              <td>Dev Mode</td>
-              <td>
-                <span className='badge badge-warning text-white'>Yes</span>
-              </td>
-            </tr>}
+              <tr>
+                <td>Name</td>
+                <td>
+                  <span className='badge badge-info'>{name}</span>
+                </td>
+              </tr>
+              <tr>
+                <td>Version</td>
+                <td>
+                  <span className='badge badge-info'>{version}</span>
+                </td>
+              </tr>
+              <tr>
+                <td>Build</td>
+                <td>
+                  <span className='badge badge-info'>{build}</span>
+                </td>
+              </tr>
+              {dev && <tr>
+                <td>Dev Mode</td>
+                <td>
+                  <span className='badge badge-warning text-white'>Yes</span>
+                </td>
+              </tr>}
             </tbody>
           </table>
         </div>
